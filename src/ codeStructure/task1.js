26d5da16d57c802e9e6dbe1da579d2734b35453c
Natalie <79932225+Nadia982@users.noreply.github.com>
2023-02-02 09:24:07 +0000

    completed code structure tasks 1-3

---
+++ src/ codeStructure/task1.js
@@ -0,0 +1,5 @@
+// I wrote the code below on one line, but VS Code automatically put it on multiple lines
+
+console.log(first);
+/*some comment*/ console.log(second);
+/*another comment*/ console.log(third);
