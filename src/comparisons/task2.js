5b5292e16e1b1ec29c151bda5c56df9511a6c054
Natalie <79932225+Nadia982@users.noreply.github.com>
2023-02-06 12:41:24 +0000

    Story 16 completed - JS comparisons

---
+++ src/comparisons/task2.js
@@ -0,0 +1,13 @@
+// * Create an expression that verifies variable a is odd or even number
+// Mathematic clue: If a number is evenly divisible by 2 with no remainder, then it is even
+// Assign to the variable a value 4
+// Print to the console the expr and verify if true was printed
+let a = 4;
+let isitEven = a % 2 === 0;
+console.log(isitEven);
+
+// Assign to the variable a value 5
+// Print to the console the expression and verify if false was printed
+let b = 5;
+isitEven = b % 2 === 0;
+console.log(isitEven);
