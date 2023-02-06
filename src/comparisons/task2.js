d6305f5c1d39c2439fd9ac053cb060c3e4f70bf5
Natalie <79932225+Nadia982@users.noreply.github.com>
2023-02-06 15:34:10 +0000

    changed let to const in comparisons task

--- src/comparisons/task2.js
+++ src/comparisons/task2.js
@@ -2,12 +2,12 @@
 // Mathematic clue: If a number is evenly divisible by 2 with no remainder, then it is even
 // Assign to the variable a value 4
 // Print to the console the expr and verify if true was printed
-let a = 4;
-let isitEven = a % 2 === 0;
+const a = 4;
+const isitEven = a % 2 === 0;
 console.log(isitEven);
 
 // Assign to the variable a value 5
 // Print to the console the expression and verify if false was printed
-let b = 5;
+const b = 5;
 isitEven = b % 2 === 0;
 console.log(isitEven);
