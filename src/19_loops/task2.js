5327f0ced203bd8c034bfd3a935f27bbb6e05873
Natalie <79932225+Nadia982@users.noreply.github.com>
2023-02-12 00:11:52 +0000

    Story 19 (loops) tasks completed

---
+++ src/19_loops/task2.js
@@ -0,0 +1,9 @@
+// 1. Create a constant title with an initial value: 'devMentor'
+// 2. Iterate through title letters and print one by one to the console
+//(read documentation about string length and how to get a letter by index)
+
+const title = "devMentor";
+
+for (let i = 0; i < title.length; i++) {
+  console.log(title[i]);
+}
